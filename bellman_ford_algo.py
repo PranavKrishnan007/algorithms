7442a2d98e6cc6f38233afe3c3325bd6a5b3e606
Pranav Krishnan <pranavkrishnan@Pranavs-MacBook-Pro.local>
2024-06-10 19:28:10 +0530

    <fix> : [bellman ford with user input]

--- bellman_ford_algo.py
+++ bellman_ford_algo.py
@@ -2,14 +2,14 @@
 
 import numpy as np
 
-s = [
-    [999, 10 , 999, 999, 999, 8],
-    [999, 999, 999, 2, 999, 999],
-    [999, 1, 999, 999, 999, 999],
-    [999, 999, -2, 999, 999, 999],
-    [999, -4, 999, -1, 999, 999],
-    [999, 999, 999, 999, 1, 999] 
-    ]
+# s = [
+#     [999, 10 , 999, 999, 999, 8],
+#     [999, 999, 999, 2, 999, 999],
+#     [999, 1, 999, 999, 999, 999],
+#     [999, 999, -2, 999, 999, 999],
+#     [999, -4, 999, -1, 999, 999],
+#     [999, 999, 999, 999, 1, 999] 
+#     ]
 
 def bellman_ford_algo(s, start, n):
     distance = [999 for i in range(n)]
@@ -27,6 +27,7 @@ def bellman_ford_algo(s, start, n):
 
     return distance
 
+n = int(input("Enter the number of nodes : "))
+s = [[int(input("Enter the matrix elements separated by spaces : ").split()) for i in range(n)] for j in range(n)]
 start = 0
-n = 6
 print(bellman_ford_algo(s, start, n))
